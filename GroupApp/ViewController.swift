8c3f3acb230d879d2886776772a9f916cb7b877f
Joe Antongiovanni <joe@Joes-Mac.local>
2018-03-07 14:26:33 -0800

    adding base project

---
+++ GroupApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GroupApp
+//
+//  Created by Joe Antongiovanni on 3/7/18.
+//  Copyright © 2018 Joe Antongiovanni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
