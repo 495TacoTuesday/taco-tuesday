9d8decced9ddc250a6c9a9e2ed47214515f6a048
Joe Antongiovanni <joe@Joes-Mac.local>
2018-03-26 14:47:03 -0700

    minor changes

---
+++ HomeMapViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HomeMapViewController.swift
+//  GroupApp
+//
+//  Created by Joe Antongiovanni on 3/26/18.
+//  Copyright © 2018 Joe Antongiovanni. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class HomeMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+													
