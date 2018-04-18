d0f02753ba07bfc68beed228a22e74f3f745c1db
Honorio Vega <hvega@csumb.edu>
2018-04-18 14:53:54 -0700

    able to add deal and shows up in database

--- AddDealViewController.swift
+++ AddDealViewController.swift
@@ -9,11 +9,20 @@
 import UIKit
 
 class AddDealViewController: UIViewController {
+    
+    
+    @IBOutlet weak var dealNameTextField: UITextField!
+    
+    @IBOutlet weak var businessNameTextField: UITextField!
+    
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +30,49 @@ class AddDealViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func didTapClose(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
+    
+    @IBAction func didTapAddDeal(_ sender: Any) {
+        
+        let dealName = dealNameTextField.text ?? ""
+        
+        let businessName = businessNameTextField.text ?? ""
 
+        let description = descriptionTextField.text ?? ""
+
+
+        let alertController = UIAlertController(title: "Sucess", message: "Deal added successfully", preferredStyle: .alert)
+
+        
+        let tryAgainAction = UIAlertAction(title: "Ok", style: .default) { (action) in
+            
+        }
+        
+        alertController.addAction(tryAgainAction)
+        
+
+        
+        // add the OK action to the alert controller
+        // Do any additional setup after loa
+        
+        Deal.postUserDeal(dealName : dealName,buisnessName: businessName,description : description,latt : 100.0,long : 100.0) { (success : Bool, error : Error?) in
+            
+            if success {
+                print("it worked")
+                
+                self.present(alertController, animated: true) {
+                    
+                }
+//                self.present(self.alertController, animated: true) {
+//
+//                }
+            }
+        }
+            
+        }
+    
+    
 }
