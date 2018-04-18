0e31ffa340232baf6bd85e2a47455ef35f65a2bc
Honorio Vega <hvega@csumb.edu>
2018-04-18 14:16:17 -0700

    added view fro add deal

---
+++ AddDealViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddDealViewController.swift
+//  GroupApp
+//
+//  Created by Honorio Vega on 4/18/18.
+//  Copyright © 2018 Joe Antongiovanni. All rights reserved.
+//
+
+import UIKit
+
+class AddDealViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
